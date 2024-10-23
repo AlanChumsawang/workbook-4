8168ca98d5fc6f74c3399eba4fec9ea94414e940
AlanChumsawang <alan.chumsawang@outlook.com>
2024-10-23 09:57:27 -0400

    Created getAllVehicles method

--- car-dealership/src/main/java/com/pluralsight/cardealership/Dealership.java
+++ car-dealership/src/main/java/com/pluralsight/cardealership/Dealership.java
@@ -13,14 +13,19 @@ public class Dealership {
         this.name = name;
         this.address = address;
         this.phone = phone;
-        this.inventory = new ArrayList<>();
+        this.inventory = new ArrayList<Vehicle>();
     }
 
     public void addVehicle(Vehicle vehicle){
         inventory.add(vehicle);
     }
 
-    public Vehicle getAllVehicals(ArrayList inventory)
+    public static void getAllVehicals(ArrayList<Vehicle> inventory){
+        for (Vehicle vehicle : inventory) {
+            System.out.println(vehicle.getYear() + " " + vehicle.getMake() + " " + vehicle.getModel() + "\n    "
+                    + vehicle.getPrice() + "\n    " + vehicle.getOdometer() + "\n    " + vehicle.getVin());
+        }
+    }
 
 
 }
--- car-dealership/src/main/java/com/pluralsight/cardealership/UserInterface.java
+++ car-dealership/src/main/java/com/pluralsight/cardealership/UserInterface.java
@@ -2,4 +2,12 @@ package com.pluralsight.cardealership;
 
 public class UserInterface {
 
+    public static void mainMenu() {
+        System.out.println("Welcome to the Car Dealership Inventory System");
+        System.out.println("1. View Inventory");
+        System.out.println("2. Add Vehicle");
+        System.out.println("3. Remove Vehicle");
+        System.out.println("4. Save Inventory");
+        System.out.println("5. Exit");
+    }
 }
