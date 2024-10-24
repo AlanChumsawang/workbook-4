83382769889a2f2eabfe85dc309ca4a3f53a7d25
AlanChumsawang <alan.chumsawang@outlook.com>
2024-10-24 09:18:57 -0400

    Added test to DealershipFileManager

--- car-dealership/src/test/java/com/pluralsight/cardealership/test/DealershipFileManagerTest.java
+++ car-dealership/src/test/java/com/pluralsight/cardealership/test/DealershipFileManagerTest.java
@@ -1,12 +1,21 @@
-package com.pluralsight.cardealership;
+package com.pluralsight.cardealership.test;
+
+import com.pluralsight.cardealership.Dealership;
+import com.pluralsight.cardealership.Vehicle;
+import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class DealershipFileManager {
+import static org.junit.jupiter.api.Assertions.*;
 
+public class DealershipFileManagerTest {
+    private Dealership dealership;
+    private Vehicle vehicle;
 
-    public void getDealership(){
+    @Test
+    void getDealership() {
+
         try {
             FileReader fileReader = new FileReader("src/main/resources/inventory.csv");
             BufferedReader bufReader = new BufferedReader(fileReader);
@@ -15,27 +24,29 @@ public class DealershipFileManager {
             String dealershipName = dealershipInfo[0];
             String dealershipAddress = dealershipInfo[1];
             String dealershipPhone = dealershipInfo[2];
-            Dealership dealership = new Dealership(dealershipName, dealershipPhone, dealershipAddress);
-            while ( (currentLine = bufReader.readLine()) != null){
+            dealership = new Dealership(dealershipName, dealershipPhone, dealershipAddress);
+            while ((currentLine = bufReader.readLine()) != null) {
                 String[] vehicleInfo = currentLine.split("[|]");
                 int vin = Integer.parseInt(vehicleInfo[0]);
                 int year = Integer.parseInt(vehicleInfo[1]);
                 String make = vehicleInfo[2];
                 String model = vehicleInfo[3];
-                String type =  vehicleInfo[4];
+                String type = vehicleInfo[4];
                 String color = vehicleInfo[5];
                 int odometer = Integer.parseInt(vehicleInfo[6]);
                 double price = Double.parseDouble(vehicleInfo[7]);
-                Vehicle vehicle = new Vehicle(vin, year, make, model, type, color, odometer, price);
+                vehicle = new Vehicle(vin, year, make, model, type, color, odometer, price);
                 dealership.addVehicle(vehicle);
             }
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println("File not found");
             throw new RuntimeException();
         }
+        assertEquals(3, dealership.getAllVehicles().size());
     }
-    public void saveDealership(Dealership dealership){
-        // Implement the method to save the dealership to a file
+
+    @Test
+    void saveDealership() {
+        // Implement the test for saving the dealership to a file
     }
 }
